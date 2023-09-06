aa51cd74dd049419e620d5f266ad2d8a0e171213
azanniel <leo.azannielttt@gmail.com>
2023-09-06 10:53:45 -0400

    ✨ create stars with skia

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,6 +5,7 @@ export const theme = {
     gray_100: '#E1E1E6',
     gray_200: '#C4C4CC',
     gray_300: '#8D8D99',
+    gray_400: '#6F6F6F',
     gray_500: '#505059',
     gray_600: '#323238',
     gray_700: '#29292E',
@@ -14,6 +15,8 @@ export const theme = {
     brand_300: '#00B37E',
     brand_400: '#1B873F',
     brand_600: '#00875F',
+
+    blue_200: '#57B6E5',
 
     warning_200: '#FBA94C',
     warning_400: '#EB8A1D',
