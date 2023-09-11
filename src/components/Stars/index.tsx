292fe7fae7f950dc04be2b21e6c37bd076ee4907
azanniel <leo.azannielttt@gmail.com>
2023-09-11 15:36:05 -0400

    ✨ animate trophy

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -1,4 +1,5 @@
 import { View } from 'react-native'
+import Animated, { BounceIn } from 'react-native-reanimated'
 import {
   Canvas,
   Easing,
@@ -141,7 +142,9 @@ export function Stars() {
         />
       </Canvas>
 
-      <TrophySvg />
+      <Animated.View entering={BounceIn}>
+        <TrophySvg />
+      </Animated.View>
     </View>
   )
 }
